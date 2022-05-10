34efd8b593e788f400daec87a76e16e73a79da07
Stefano Dobbin <sndobbin@gmail.com>
2022-05-10 07:00:05 +0100

    Alpha_0.7.2
    - Testing stuff

--- Source/GEIIProject/GEIIProjectFunctionLibrary.cpp
+++ Source/GEIIProject/GEIIProjectFunctionLibrary.cpp
@@ -30,3 +30,63 @@ void UGEIIProjectFunctionLibrary::ResizeRenderTarget(UTextureRenderTarget2D* Ren
 
 	RenderTarget->ResizeTarget(SizeX, SizeY);
 }
+
+FVector UGEIIProjectFunctionLibrary::ConvertLocation(FVector const& Location, AActor* Portal, AActor* Target)
+{
+	FVector Direction = Location - Portal->GetActorLocation();
+	FVector TargetLocation = Target->GetActorLocation();
+
+	FVector Dots;
+
+	Dots.X = FVector::DotProduct(Direction, Portal->GetActorForwardVector());
+	Dots.Y = FVector::DotProduct(Direction, Portal->GetActorRightVector());
+	Dots.Z = FVector::DotProduct(Direction, Portal->GetActorUpVector());
+
+	FVector NewDirection = Dots.X * -Target->GetActorForwardVector() +
+						   Dots.Y * -Target->GetActorRightVector() +
+						   Dots.Z * Target->GetActorUpVector();
+
+	return TargetLocation + NewDirection;
+}
+
+FRotator UGEIIProjectFunctionLibrary::ConvertRotation(FRotator const& Rotation, AActor* Portal, AActor* Target)
+{
+	FVector RotationAdjustment(0.0f, 0.0f, -180.0f);
+	FVector LocalAdjustment = FVector::ZeroVector;
+
+	if(FVector::DotProduct(Portal->GetActorForwardVector(), FVector::UpVector) > KINDA_SMALL_NUMBER)
+	{
+		LocalAdjustment.X = FMath::UnwindDegrees(Portal->GetTransform().GetRotation().Euler().X);
+		LocalAdjustment.Y = 180.0f;
+		RotationAdjustment.Z += LocalAdjustment.X;
+	}
+	else if(FVector::DotProduct(Portal->GetActorForwardVector(), FVector::UpVector) < -KINDA_SMALL_NUMBER)
+	{
+		LocalAdjustment.X = FMath::UnwindDegrees(Portal->GetTransform().GetRotation().Euler().X);
+		LocalAdjustment.Y = -180.0f;
+		RotationAdjustment.Z += LocalAdjustment.X;
+	}
+
+	FQuat QuatRotation = FQuat::MakeFromEuler(RotationAdjustment) * FQuat(Rotation);
+	FQuat LocalQuat = FQuat::MakeFromEuler(LocalAdjustment) *
+		Portal->GetActorTransform().GetRotation().Inverse() * QuatRotation;
+
+	FQuat NewWorldQuat = Target->GetActorTransform().GetRotation() * LocalQuat;
+	
+	return NewWorldQuat.Rotator();
+}
+
+bool UGEIIProjectFunctionLibrary::CheckIsInFront(FVector const& Point, FVector& PortalLocation, FVector const& PortalNormal)
+{
+	FPlane PortalPlane = FPlane(PortalLocation, PortalNormal);
+	
+	return PortalPlane.PlaneDot(Point) >= 0;
+}
+
+/*bool UGEIIProjectFunctionLibrary::CheckIsCrossing(FVector const& Point, FVector& PortalLocation, FVector const& PortalNormal, bool& out_LastInFront, FVector& out_LastPosition)
+{
+	FPlane PortalPlane = FPlane(PortalLocation, PortalNormal);
+	FVector IntersectionPoint;
+
+	bool IsIntersecting = FMath::SegmentPlaneIntersection(out_last)
+}*/
